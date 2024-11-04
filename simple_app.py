13783548a6e2471822031372654ad30639ab9e5a
colo134 <160218330+colo134@users.noreply.github.com>
2024-11-04 09:08:39 +0530

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -4,11 +4,11 @@ import plotly.express as px
 import pandas as pd
 
 
-# # Load the tips dataset from Seaborn
-# tips = sns.load_dataset('tips')  # Loading the dataset
+# Load the tips dataset from Seaborn
+tips = sns.load_dataset('tips')  # Loading the dataset
 
-# # Display the first few rows of the dataset
-# print(tips.head())  # This will show the first 5 rows of the tips dataset
-#Visualizations using Plotly
+# Display the first few rows of the dataset
+print(tips.head())  # This will show the first 5 rows of the tips dataset
+# Visualizations using Plotly
 fig1 = px.bar(tips, x="day", y="tip")
 fig1.show()
