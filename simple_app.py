dd87fde3dcb0f4aabfa4616a5bf50ee718fa0f53
colo134 <160218330+colo134@users.noreply.github.com>
2024-11-04 09:04:14 +0530

    Add files via upload

---
+++ simple_app.py
@@ -0,0 +1,11 @@
+# Import necessary libraries
+import seaborn as sns  # Importing Seaborn library for data visualization
+import plotly.express as px
+import pandas as pd
+
+
+# # Load the tips dataset from Seaborn
+# tips = sns.load_dataset('tips')  # Loading the dataset
+
+# # Display the first few rows of the dataset
+# print(tips.head())  # This will show the first 5 rows of the tips dataset
